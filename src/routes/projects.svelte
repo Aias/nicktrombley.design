4370dda6320a0c184ed8753a4d85f5340aed20f4
Nick Trombley <trombley.nick@gmail.com>
2019-10-06 14:06:55 -0400

    Initial projects structuring

--- src/routes/projects.svelte
+++ src/routes/projects.svelte
@@ -27,13 +27,51 @@
 
 	export let projects = undefined;
 
+	let tags = {};
+	let fields = {};
+	let roles = {};
+	let technologies = {};
+
 	$: {
 		console.log(projects);
+		projects.forEach((project, i) => {
+			mapProperty(project, tags, 'tags');
+			mapProperty(project, fields, 'field');
+			mapProperty(project, roles, 'role');
+			mapProperty(project, technologies, 'technologies');
+		});
 	}
+
+	const mapProperty = (source, target, prop) => {
+		source[prop].forEach(item => {
+			if (typeof target[item] === 'object') {
+				target[item].count++;
+			} else {
+				target[item] = { count: 1 };
+			}
+		});
+	};
 </script>
 
 <PageTitle>The Work List</PageTitle>
 
 <article class="layout__all">
-	Under construction.
+	<section>
+		<h3>If you're looking for a...</h3>
+		<fieldset>
+			<label>
+				<input type="checkbox" />
+				Designer
+			</label>
+		</fieldset>
+	</section>
+	<section></section>
 </article>
+
+<style>
+	fieldset {
+		margin: 0.5rem 0;
+		padding: 0;
+		border: none;
+	}
+</style>
