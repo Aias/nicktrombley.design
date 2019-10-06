8e7c886c097220365f6cf81adf60e1ec8eebd5a3
Nick Trombley <trombley.nick@gmail.com>
2019-10-06 10:40:33 -0400

    Upate meta tag stuff and site logo

--- src/components/SEO.svelte
+++ src/components/SEO.svelte
@@ -1,15 +1,36 @@
 <script>
-	export let title = 'nick trombley . design';
+	export let siteName = 'nick trombley . design';
+	export let title = siteName;
+	export let subject = '';
 	export let description = 'a portfolio of sorts';
 	export let keywords =
 		'Nick Trombley, design, resume, CV, portfolio, work, development, front-end, web';
+	export let author = 'Nick Trombley';
+	export let email = 'trombley.nick@gmail.com';
+	export let type = 'website'; // article, profile
+	export let image = '/logo-512.png';
+	export let themeColor = '#1f0066';
+	export let robots = 'index, follow';
+	export let url = 'https://nicktrombley.design';
 </script>
 
 <svelte:head>
+	<!-- Meta basics -->
 	<title>{title}</title>
-	<meta property="og:title" content="{title}" />
-	<meta name="Description" content="{description}" />
-	<meta property="og:description" content="{description}" />
-	<meta name="keywords" content="{keywords}" />
-	<meta name="theme-color" content="#1f0066" />
+	<meta name="description"		content="{description}" />
+	{#if subject}
+	<meta name="subject"			content="{subject}" />
+	{/if}
+	<meta name="keywords"			content="{keywords}" />
+	<meta name="theme-color"		content="{themeColor}" />
+	<meta name="robots"				content="{robots}" />
+	<meta name="author"				content="{author}">
+	<meta name="reply-to"			content="{email}">
+	<meta name="url"				content="{url}">
+	<!-- OpenGraph tags -->
+	<meta property="og:site_name"	content="{siteName}" />
+	<meta property="og:type"		content="{type}"/>
+	<meta property="og:title"		content="{title}" />
+	<meta property="og:description"	content="{description}" />
+	<meta property="og:image"		content="{image}" />
 </svelte:head>
