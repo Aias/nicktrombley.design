a1a4e67138272d5d870a59fb6a819a16d2b873a5
Nick Trombley <trombley.nick@gmail.com>
2025-03-22 18:27:53 -0400

    Fix desaturation filter and add grid background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -27,7 +27,7 @@
 	];
 </script>
 
-<div class="widgets-container">
+<div class="widgets-container grid-background">
 	{#each widgets as widget}
 		<div
 			class="widget"
@@ -45,11 +45,56 @@
 <style>
 	.widgets-container {
 		position: relative;
+		width: 2304px;
+		height: 1536px;
 	}
 	.widget {
 		position: absolute;
+		opacity: 0.64;
+		transition: opacity 0.3s ease;
+
+		&:hover {
+			opacity: 1;
+		}
 	}
 	.widget :global(svg) {
 		fill: transparent;
+	}
+
+	.grid-background {
+		background-color: transparent;
+		background-image:
+    /* Larger gridlines every 32px with 33% opacity */
+			linear-gradient(
+				color-mix(in srgb, var(--widget-divider) 33%, transparent) 1px,
+				transparent 1px
+			),
+			linear-gradient(
+				90deg,
+				color-mix(in srgb, var(--widget-divider) 33%, transparent) 1px,
+				transparent 1px
+			),
+			/* Smaller gridlines every 8px with 33% opacity */
+				linear-gradient(
+					color-mix(in srgb, var(--widget-divider-subtle) 33%, transparent) 1px,
+					transparent 1px
+				),
+			linear-gradient(
+				90deg,
+				color-mix(in srgb, var(--widget-divider-subtle) 33%, transparent) 1px,
+				transparent 1px
+			);
+
+		background-size:
+			32px 32px,
+			/* Larger vertical and horizontal grid size */ 32px 32px,
+			/* Larger vertical and horizontal grid size */ 8px 8px,
+			/* Smaller vertical and horizontal grid size */ 8px 8px; /* Smaller vertical and horizontal grid size */
+
+		background-position:
+			0 0,
+			/* Larger vertical gridlines position */ 0 0,
+			/* Larger horizontal gridlines position */ 0 0,
+			/* Smaller vertical gridlines position */ 0 0; /* Smaller horizontal gridlines position */
 	}
 </style>
